58e02b57e36f71372932ce5e454bd7c76c38345a
mariko1216 <marichan1216@gmail.com>
2025-09-13 22:39:55 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(".img").slick({
+    arrows: false,
+    centerMode: true,
+    centerPadding: "100px",
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          centerPadding: "50px",
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
